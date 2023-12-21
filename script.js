b4a74bd841715c913f404844f5ef24256054de5d
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-22 00:54:27 +0500

    ternary

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num="";
+/*let num="";
 
 if(num){
     console.log('num is truthy value')
@@ -6,3 +6,76 @@ if(num){
 else{
     console.log(`  ${num} is falsy value`)
 }
+
+
+let x=prompt(`choose your fav number`);
+
+if(x===2){
+console.log(typeof x)
+}else if(x==2){
+    console.log(typeof x)
+}
+
+console.log('10' === '10')
+
+console.log('12'== 12)
+
+let x='2'
+
+
+if(x!= 2){ // 2 != '2'
+console.log('hi')
+}else{
+    console.log('ye wala dkho')
+}
+
+
+
+let x=2;
+
+if(x===2){
+    console.log(`this one`)
+} else if(x== 2){
+    console.log(`genuinely this one`)
+}
+
+
+
+let myAge =5;
+
+if(myAge >=10 || myAge<= 30){
+    console.log(myAge)
+}
+
+
+
+let rollnum = 1991;
+console.log(rollnum+true);
+
+
+console.log(2+true)
+
+
+console.log(2-'s')
+
+*/
+
+let sarahAge= 18;
+let hasDrivingExperience='';
+
+if(sarahAge >= 18 && hasDrivingExperience){
+    console.log(`only she can drive then`)
+}
+//challenge2
+let scoreDolphins= (96+ 108+ 89)/3;
+let scoreKoalas =  (88+ 91+  110)/3
+
+if(scoreDolphins>scoreKoalas){
+    console.log(`Dolphins win the trophy`)
+}else if(scoreKoalas>scoreDolphins){
+    console.log(`Koalas win the trophy`)
+}else{
+    console.log(`Both win the trophy`)
+}
+
+
