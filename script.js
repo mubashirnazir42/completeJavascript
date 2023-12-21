1653db803d1a37d124fce3978e3920f90700a840
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-21 05:02:59 +0500

    third commit

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ let x= 100;
  x--;
 
  console.log(x)
+
+
+
+
+ let $mubashirAge =25;
+ let _myFriendAge = 28;
+
+ console.log($mubashirAge > _myFriendAge? "mubashir is younger" : "mubashir is elder")
+
+
+ let age=4;
+ age += 23;
+
+ console.log(age-2)
+ console.log(age)
+
+ console.log(age> 23)
+
+ let myAage =  age>23;
+ console.log(myAage)
+ console.log("munna")
+ let munna = hello+3; //4+3
+ console.log(munna) //7
+ console.log(munna+2) // 9
+ console.log(munna) // 7
+ console.log( munna*2 ) 
+
+
+let m,y;
+
+m= y= 10-7;
+console.log(m,y)
+
+
