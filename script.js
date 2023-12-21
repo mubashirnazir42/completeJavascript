93e74ca87f2d4163c1b13bbe993d6a95c5370692
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-21 05:17:15 +0500

    fourth commit

--- script.js
+++ script.js
@@ -1,57 +1,14 @@
-let $variable = "3.14";
-let _minus1 = -1;
+let m=4 , l=9 , n=24;
+
+let result=y =  n  -m
 
 
-let hello = true;
-
-console.log(hello)
-
-
-hello= 4;
-
-console.log(hello);
-
-let x= 100;
-
- x += 2;
-
- console.log(x)
- 
- x--;
- x--;
-
- console.log(x)
+console.log(result,y)
 
 
 
-
- let $mubashirAge =25;
- let _myFriendAge = 28;
-
- console.log($mubashirAge > _myFriendAge? "mubashir is younger" : "mubashir is elder")
+let ageMubashir = 25;
+let ageShaffan = 29;
 
 
- let age=4;
- age += 23;
-
- console.log(age-2)
- console.log(age)
-
- console.log(age> 23)
-
- let myAage =  age>23;
- console.log(myAage)
- console.log("munna")
- let munna = hello+3; //4+3
- console.log(munna) //7
- console.log(munna+2) // 9
- console.log(munna) // 7
- console.log( munna*2 ) 
-
-
-let m,y;
-
-m= y= 10-7;
-console.log(m,y)
-
-
+console.log((ageMubashir+ageShaffan)/2)
