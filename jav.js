e823c1229c86e2601c38929f8a903a9ddd5fcd30
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2023-12-23 18:16:26 +0500

    functionmessage

--- jav.js
+++ jav.js
@@ -1,142 +1,114 @@
-// let num="";
-
-// if(num){
-//     console.log('num is truthy value')
-// }
-// else{
-//     console.log(`  ${num} is falsy value`)
-// }
-
-
-//  let x=`choose your fav number`;
-
-// if(x===2){
-// console.log(typeof x)
-// }else if(x==2){
-//     console.log(typeof x)
-// }
-
-// console.log('10' === '10')
-
-// console.log('12'== 12)
-
-// // let x='2'
-
-
-// if(x!= 2){ // 2 != '2'
-// console.log('hi')
-// }else{
-//     console.log('ye wala dkho')
-// }
+'use strict';
+console.log(juiceProducer(2,3))
+function juiceProducer(apple,orange){
+    return `juice of ${apple} APPLES and ${orange} oranges`;
+}
+let machine = 2;
 
 
 
-// // let x=2;
-
-// if(x===2){
-//     console.log(`this one`)
-// } else if(x== 2){
-//     console.log(`genuinely this one`)
-// }
-
-
-
-// let myAge =5;
-
-// if(myAge >=10 || myAge<= 30){
-//     console.log(myAge)
-// }
-
-
-
-// let rollnum = 1991;
-// console.log(rollnum+true);
-
-
-// console.log(2+true)
-
-
-// console.log(2-'s')
-
-
-
-// let sarahAge= 18;
-// let hasDrivingExperience='';
-
-// if(sarahAge >= 18 && hasDrivingExperience){
-//     console.log(`only she can drive then`)
-// }
-
-
-// //challenge2
-// let scoreDolphins= (97+ 112+ 81)/3;
-// let scoreKoalas =  (109+ 95+  86)/3;
-
-// console.log(scoreDolphins, scoreKoalas)
-
-// if(scoreDolphins>scoreKoalas && scoreDolphins>=100){
-//     console.log(`Dolphins win the trophy`)
-// }else if(scoreKoalas>scoreDolphins && scoreKoalas>=100){
-//     console.log(`Koalas win the trophy`)
-// }else if(scoreDolphins === scoreKoalas){
-//     console.log(`Both win the trophy`)
-// }else{
-//     console.log(`no one wins`)
-// }
-// let age=19;
-
-
-// console.log(x)
-
-// console.log(`i am mubashir and i am ${age>=18? 'adult' : 'young'}`);
-
-
-// // Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
-
-// // Your tasks:
-
-// // Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
-
-// // Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
-
-// // Example: The bill was 275, the tip was 41.25, and the total value 316.25.
-
-// // Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
-
-// // TEST DATA: Test with different bill values: 275, 40, and 430
-
-// // HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
-
-// // HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
-
-
-// let bill= 20;
-
-// let tip = bill>=50 && bill<=300? 0.15*bill :0.2*bill;
-
-// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}.`);
-
-// let day = 'thirsday';
-
-// switch(day){
-//     case 'monday' : console.log('mon'); break;
-//     case 'tuesday' : console.log('tues'); break;
-//     case 'wednesday' : console.log('wed'); break;
-//     case 'thirsday':
-//         case 'friday' : console.log('off'); break;
-//     default : console.log('hahah')
-// }
-
-
-let day  ;
-
-switch('mub'){
-
-    case 0 : console.log('hahaha'); break;
-    case 1 : console.log('muahahha'); break;
-    case 'mub' : console.log('tahahaha'); break;
-    case 3 : console.log('wahahahhaa'); break;
-    case 4 : console.log('thahahahahaha'); break;
+function multiply(x,y,z){
     
+return x*y;
+}
+
+console.log(juiceProducer(4,5))
+console.log(machine)
+console.log(juiceProducer(4,5))
+console.log(++machine)
+console.log(juiceProducer(4,5))
+console.log(juiceProducer(4,5))
+console.log(machine)
+
+
+
+function  fruitJuice(milk,mango){
+    const juice= `the milk shake made by ${milk} glass of milk  and ${mango} chop mangoes`;
+    return juice;
 
 }
+
+
+
+
+console.log(fruitJuice(2,4))
+console.log(fruitJuice(6,8))
+
+function  ternery(speed){
+ const x= speed<=40? 'slow' : speed <=60?'average'  : 'fast'
+ return x;
+}
+
+console.log(ternery(41))
+
+
+
+
+
+
+
+const calcAge= function(bd,crrntYr){
+
+
+const x= crrntYr-bd;
+return x;
+
+    
+}
+
+
+
+  const currentAge = function(birthYear,currentYear,retirements){
+   const age=calcAge(birthYear,currentYear)
+   
+   const remainingService= retirements - age;
+   return `my age is ${age} and remaining years in retirement is in ${remainingService}`;
+  }
+  console.log(currentAge(1994,2023,60))
+  console.log(currentAge(1998,2023,60))
+  console.log(currentAge(2002,2023,60))
+  console.log(currentAge(2012,2023,60))
+  const calcAverage = (score1,score2,score3) => (score1+score2+score3)/3;
+console.log(calcAverage(44,23,71))
+
+
+const calcAverages = (score1,score2,score3) => (score1+score2+score3)/3;
+  let scoreDolpshins=calcAverages(44,23,71)
+  let scoreKoalas=calcAverages(65,54,49);
+
+  
+  
+const checkWinner = function(avgDolphins,avgKoalas){
+    
+const x=    avgDolphins>= 2*avgKoalas? `Dolphin wins 🏆( ${avgDolphins} vs ${avgKoalas} )` : avgKoalas>= 2*avgDolphins? `koalas wins (${avgKoalas} vs${avgDolphins}) ` : 'No team wins';
+return x;
+}
+
+console.log(checkWinner(scoreDolphins,scoreKoalas))
+
+let calcAges= function(currentYear,birthYear){ 
+    const res=currentYear-birthYear;
+    return res;
+}
+
+let age= calcAges(2023,1994);
+
+
+
+const retirement = function(currentYear,  dob,Sixty,name){
+
+   const age= calcAge(currentYear,dob)
+    
+const x= `retirment of mine is ${Sixty-age}, name=> ${name} `;
+return x;
+}
+console.log(retirement(2023,1995,60,'mubashir')) // currentYear,dob,limitAge
+console.log(retirement(2023,1986,60 , 'adnan nazir')) // currentYear,dob,limitAge
+console.log(retirement(2023,2005,60,'soleha')) // currentYear,dob,limitAge
+console.log(retirement(2023,1999,60,'ayesha')) // currentYear,dob,limitAge
+
+
+
+
+
+
